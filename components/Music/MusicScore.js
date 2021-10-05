c4acaefeefe12751d24cb2ef6efd7cdd3f0328bf
Sherwin <sherwinzhang100@gmail.com>
2021-10-05 15:43:11 -0700

    update gitignore and less

--- components/Music/MusicScore.js
+++ components/Music/MusicScore.js
@@ -1,12 +1,13 @@
 import {Component} from 'react'
 import Abc2Svg from 'react-abc2svg'
 
+
 class MusicScore extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return <div> test score
+        return <div className=''> test score
             <Abc2Svg
                 abcNotation={this.props.abc}
                 showErrors
